e79639ae46d3ad430ef3b4f337054b2a95dda72e
MSiorr <michaldakol@gmail.com>
2023-04-01 21:12:58 +0200

    try to add map
    yep

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _maxspeed = 200f;
     CharacterController playerController;
+
+    [SerializeField]
+    Rigidbody2D _rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = transform.forward * Time.deltaTime * _speed;
+        //Vector3 move = transform.forward * Time.deltaTime * _speed;
+
         transform.Rotate(0, 0, Input.GetAxis("Horizontal") * 40f * -Time.deltaTime * 5);
         float velocity = 0;
         velocity = velocity + _speed * Time.deltaTime;
@@ -29,7 +34,14 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.up * velocity;
+            //transform.position += transform.up * velocity;
+            Vector3 localVelocity = Vector3.ClampMagnitude(new Vector3(0, velocity, 0), 1) * _speed;
+
+            _rb2d.velocity = transform.TransformDirection(localVelocity) * _speed;
+
+        } else
+        {
+            _rb2d.velocity = new Vector2(0, 0);
 
         }
 
