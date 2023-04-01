064609770d0e17019e5b3c3bf82307351f6ea2fa
MSiorr <michaldakol@gmail.com>
2023-04-01 21:19:58 +0200

    undo player script changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _maxspeed = 200f;
     CharacterController playerController;
-
-    [SerializeField]
-    Rigidbody2D _rb2d;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +18,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 move = transform.forward * Time.deltaTime * _speed;
-
+        Vector3 move = transform.forward * Time.deltaTime * _speed;
         transform.Rotate(0, 0, Input.GetAxis("Horizontal") * 40f * -Time.deltaTime * 5);
         float velocity = 0;
         velocity = velocity + _speed * Time.deltaTime;
@@ -34,14 +29,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            //transform.position += transform.up * velocity;
-            Vector3 localVelocity = Vector3.ClampMagnitude(new Vector3(0, velocity, 0), 1) * _speed;
-
-            _rb2d.velocity = transform.TransformDirection(localVelocity) * _speed;
-
-        } else
-        {
-            _rb2d.velocity = new Vector2(0, 0);
+            transform.position += transform.up * velocity;
 
         }
 
