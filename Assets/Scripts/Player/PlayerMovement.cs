6c1dbcf7c7ec4d5e5ec87fc4fd4286e000dab020
Jakub Kwiecień <kkwiecie@interia.pl>
2023-04-01 22:55:22 +0100

    fixed deceleration

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,25 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     [SerializeField]
     private float _speed = 5f;
     [SerializeField]
     private float _maxspeed = 200f;
 
-    void Update() {
+    void Update()
+    {
         transform.Rotate(0, 0, Input.GetAxis("Horizontal") * 40f * -Time.deltaTime * 5);
         float velocity = 0;
-        velocity = velocity + _speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+            velocity += _speed * Time.deltaTime;
+        else
+            velocity -= _speed * Time.deltaTime;
 
-        if (velocity > _maxspeed) {
+        if (velocity > _maxspeed)
+        {
             velocity = _maxspeed;
+        } else if (velocity < 0)
+        {
+            velocity = 0;
         }
+            
 
-        if (Input.GetKey(KeyCode.W)) {
-            transform.position += transform.up * velocity;
+        transform.position += transform.up * velocity;
 
-        }
 
     }
 }
