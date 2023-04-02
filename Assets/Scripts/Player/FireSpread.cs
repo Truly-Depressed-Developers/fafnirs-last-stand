3c888b06423cc22bd52a0164fad5b5c38d05d410
Spookyless <spookyless.info@gmail.com>
2023-04-02 07:17:05 +0200

    Merged scenes

--- Assets/Scripts/Player/FireSpread.cs
+++ Assets/Scripts/Player/FireSpread.cs
@@ -2,31 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireSpread : MonoBehaviour
-{
+public class FireSpread : MonoBehaviour {
     [SerializeField]
     private GameObject containerFlame;
     [SerializeField]
     private GameObject particle;
 
-    
+
     private bool canSpawn = true;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             canSpawn = true;
             StartCoroutine(SpawnParticlesRoutine());
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
+        if (Input.GetKeyUp(KeyCode.Space)) {
             canSpawn = false;
         }
         /*if (Input.GetKey(KeyCode.Space))
@@ -45,24 +40,21 @@ public class FireSpread : MonoBehaviour
         }*/
     }
 
-    IEnumerator SpawnParticlesRoutine()
-    {
-        while (canSpawn)
-        {
+    IEnumerator SpawnParticlesRoutine() {
+        while (canSpawn) {
             Debug.Log("Test");
-            for (int i = 0; i < 3; i++)
-            {
+            for (int i = 0; i < 3; i++) {
                 GameObject particleObj = Instantiate(particle, transform.position, Quaternion.identity);
                 particleObj.transform.parent = containerFlame.transform;
                 float initialAngle = transform.rotation.eulerAngles.z + Random.Range(-10f, 10f);
-                
-                particleObj.transform.rotation = Quaternion.Euler(0, 0, initialAngle + 180f);
+
+                particleObj.transform.rotation = Quaternion.Euler(0, 0, initialAngle);
             }
-            
+
             yield return new WaitForSeconds(0.1f);
-        } 
+        }
         yield return null;
-        
+
 
     }
 
