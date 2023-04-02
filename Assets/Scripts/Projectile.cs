c27351faf4f15ec2a613bc8e9f299037c65ef63c
Rafał Jodłowski <rafaljodlowski24@gmail.com>
2023-04-02 08:25:57 +0200

    rotacja toporełka

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,9 +8,23 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public Vector3 movement;
+    private Vector3 rotation;
+
+    private void Start()
+    {
+        if (gameObject.transform.position.x - PlayerPosition.Instance.transform.position.x < 0)
+        {
+            rotation = Vector3.back;
+        }
+        else
+        {
+            rotation = Vector3.forward;
+        }
+    }
 
     void Update()
     {
+        gameObject.transform.Rotate(rotation, Space.Self);
         gameObject.transform.position += movement;
     }
 }
