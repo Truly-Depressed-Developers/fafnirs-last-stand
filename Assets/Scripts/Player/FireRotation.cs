1ba2d6d258aaea17e0df70a76688dfe815fdc969
Jakub Kwiecień <kkwiecie@interia.pl>
2023-04-02 01:45:25 +0100

    smooth fire rotation working properly

--- Assets/Scripts/Player/FireRotation.cs
+++ Assets/Scripts/Player/FireRotation.cs
@@ -9,6 +9,8 @@ public class FireRotation : MonoBehaviour
     [SerializeField]
     float secondAngle = 110;
 
+    float prevAngle = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,15 +23,12 @@ public class FireRotation : MonoBehaviour
         Vector3 cameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 vector = new Vector2(cameraPos.x, cameraPos.y);
         Vector2 pos = new Vector2(transform.position.x, transform.position.y);
-        float angle = Vector2.Angle(vector - pos, transform.up);
-        transform.right = vector - pos;
-        transform.RotateAround(transform.forward, -Mathf.PI / 2);
-        Vector3 euler = transform.localRotation.eulerAngles;
-        //Vector3 euler = transform.parent.eulerAngles - transform.eulerAngles;
-        if (euler.z > 180) euler.z = euler.z - 360;
-        euler.z = Mathf.Clamp(euler.z, firstAngle, secondAngle);
-        transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, euler.z);
-
+        float angle = Vector2.SignedAngle(vector - pos, transform.parent.up);
+        float dif = angle - prevAngle;
+        float newAngle = prevAngle + dif * 5f * Time.deltaTime;
+        newAngle = Mathf.Clamp(newAngle, firstAngle, secondAngle);
+        prevAngle = newAngle;
+        transform.localRotation = Quaternion.Euler(transform.localRotation.x, transform.localRotation.y, -newAngle);
 
 
     }
